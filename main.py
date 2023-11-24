b08878c38afd21e2ff8a11b7eda2f373eff6b21e
waggnerl <wagnermflira@gmail.com>
2023-11-24 09:21:29 -0300

    asdfnlkansdf

--- main.py
+++ main.py
@@ -20,9 +20,9 @@ from telethon import TelegramClient, events
 print({"API_ID": api_id, "API_HASH": api_hash, "Chat 1": chat_1, "Chat 2": chat_2, "Chat 3": chat_3, "Ably Token": ably_token, "Ably Chanel": ably_chanel, "Ably Publish": ably_publish})
 client = TelegramClient('anon', api_id, api_hash)
 client.connect()
+client.sign_in(phone=api_phone)
 me = client.get_me()
 print("Usuário autenticado:", me.stringify())
-client.sign_in(phone=api_phone)
 
 @client.on(events.NewMessage(chats = [chat_1, chat_2, chat_3]))
 async def handler(event):
