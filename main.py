e0ca93b176578d953235762ad1775c9bc9e025fe
waggnerl <wagnermflira@gmail.com>
2023-11-24 09:12:19 -0300

    dasdafasdf

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import os
 
 api_id = "22165656"
 api_hash = "e7ffdb2615ba7651095641331679943f"
+api_phone = os.getenv("api_phone") 
 ably_token = os.getenv("ably_token") 
 ably_chanel = os.getenv("ably_chanel") 
 ably_publish = os.getenv("ably_publish") 
@@ -17,7 +18,7 @@ chat_3 = os.getenv("chat_3")
 from telethon import TelegramClient, events
 
 print({"API_ID": api_id, "API_HASH": api_hash, "Chat 1": chat_1, "Chat 2": chat_2, "Chat 3": chat_3, "Ably Token": ably_token, "Ably Chanel": ably_chanel, "Ably Publish": ably_publish})
-client = TelegramClient('anon', api_id, api_hash)
+client = TelegramClient('anon', api_id, api_hash,phone=api_phone)
 @client.on(events.NewMessage(chats = [chat_1, chat_2, chat_3]))
 async def handler(event):
     print("Event Occured")
