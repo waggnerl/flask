eb4a156a935d0d21d54b0dd4beb252c7e67153a2
waggnerl <wagnermflira@gmail.com>
2023-11-24 08:21:00 -0300

    default_project

---
+++ main.py
@@ -0,0 +1,33 @@
+from telethon.sync import TelegramClient
+
+from ably import AblyRealtime
+import os
+
+
+api_id = os.getenv("api_id") 
+api_hash = os.getenv("api_hash")
+ably_token = os.getenv("ably_token") 
+ably_chanel = os.getenv("ably_chanel") 
+ably_publish = os.getenv("ably_publish") 
+chat_1 = os.getenv("chat_1") 
+chat_2 = os.getenv("chat_2") 
+chat_3 = os.getenv("chat_3") 
+
+
+from telethon import TelegramClient, events
+
+
+client = TelegramClient('anon', api_id, api_hash)
+@client.on(events.NewMessage(chats = [chat_1, chat_2, chat_3]))
+async def handler(event):
+    print("Event Occured")
+    print(event.raw_text)
+    ably = AblyRealtime(ably_token)
+
+    await ably.connection.once_async('connected')
+    print('Connected to Ably')
+    channel = ably.channels.get(ably_chanel)
+    await channel.publish(ably_publish, event.raw_text)
+    # asyncio.run(main())
+client.start()
+client.run_until_disconnected()
